0fb021b05030afb8434840e564ccbc617ad3abc7
Thomas Rogg <thomas@neonious.com>
2019-10-29 23:49:40 +0100

    api

--- include/low_native.h
+++ include/low_native.h
@@ -18,29 +18,31 @@ typedef enum
     LOW_THREAD_CODE,
     LOW_THREAD_WORKER_FAST,
     LOW_THREAD_WORKER_SLOW
-} low_thread_type;
+} low_thread;
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-__attribute__ ((visibility ("default"))) int module_main(low_context *ctx, const char *module_path);
+__attribute__ ((visibility ("default"))) bool module_load(low_context *ctx, const char *module_path);
+__attribute__ ((visibility ("default"))) bool module_unload();
 
-// Typically, in a native module parent_on_stack will be false
 void low_load_module(low_context *ctx, const char *path, bool parent_on_stack);
 
-void low_call_direct(low_context *ctx, low_thread_type thread, void (*func)(void *userdata), void *userdata);
-low_thread_type low_current_thread_type(low_context *ctx);
+void low_call_direct(low_context *ctx, low_thread thread, void (*func)(void *userdata), void *userdata);
+void low_set_socket_events(low_context *ctx, int events, void (*func)(void *userdata), void *userdata);
+low_thread low_get_current_thread(low_context *ctx);
 
 int low_set_timeout(low_context *ctx, int index, int delay, void (*call)(void *data), void *data);
 void low_clear_timeout(low_context *ctx, int index);
+
+void low_call_next_tick(low_context *ctx, int num_args);
 
 int low_add_stash(low_context *ctx, int index);
 void low_remove_stash(low_context *ctx, int index);
 void low_push_stash(low_context *ctx, int index, bool remove);
 
 void *low_push_buffer(low_context *ctx, int len);
-void low_call_next_tick(low_context *ctx, int num_args);
 
 void *low_alloc_throw(low_context *ctx, size_t size);
 
